c4780e9bde660753494c4397fe6fea0482fdf9fe
Mikael <kiamel76@gmail.com>
2023-08-28 13:35:41 +0200

    Established connection to headless wp (elementorprotraining)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//This is only a simple function to fetch from a WP and using it as headless. Connection is working and this could later be developed for the recipe-app that I was thinking about.
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://elementorprotraining.fotografmikaeljonsson.se/wp-json/wp/v2/test'
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hey</h1>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }}></h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+          ></div>
+        </div>
+      ))}
     </div>
   );
 }
